821872aada955637affd64eb8d16bd1efd57db50
Pablo Ramon <pabloramonbarros@gmail.com>
2023-09-27 21:42:48 -0300

    Alterando o Lista_de_compras.py

--- Lista_de_compras.py
+++ Lista_de_compras.py
@@ -1,13 +1,14 @@
+#importar 'os' para limpar o terminal 
 import os
-
+#cria lista vazia 
 lista_compras = []
 
-
+#cria função para adicionar itens na lista
 def adicionar_item(item, lista):
     lista.append(item)
     print(f'\n{item} foi adicionado a lista.')
 
-
+#cria função para remover itens da lista
 def retirar_item(item, lista):
     if item in lista:
         lista.remove(item)
@@ -16,7 +17,7 @@ def retirar_item(item, lista):
         print(
             f'\n{item} não está na lista,nenhum item foi retirado.')
 
-
+#cria função que exibe os itens colocados na lista
 def exibir_lista(lista):
     print('\nlista de compras:\n')
     i = 1
@@ -25,16 +26,16 @@ def exibir_lista(lista):
         print(f'{i}- {item}')
     print()
 
-
+#Menu de interação com o usuário 
 while True:
     print("Opções:")
     print('1) Adicionar item à lista:')
     print('2) Remova um item da lista:')
     print('3) Exibir a lista:')
     print('4) Sair:')
-
+#entrada para interagir com o menu
     escolha = input('Escolha uma das opções: ')
-
+#condicionais para as funções criadas
     if escolha == '1':
         os.system('cls')
         adc_item = input(
